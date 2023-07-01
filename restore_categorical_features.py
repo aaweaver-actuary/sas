3719f86c0f8d532ca56f6c828551e8339dbec2a3
aaweaver-actuary <53792857+aaweaver-actuary@users.noreply.github.com>
2023-07-01 09:48:42 -0400

    updated documentation/revised using polars

--- restore_categorical_features.py
+++ restore_categorical_features.py
@@ -1,39 +1,96 @@
-import duckdb
+import os
 import pandas as pd
-from sas7bdat import SAS7BDAT
+import polars as pl
+import psutil
 
-# Lazily connect to the CSV files using DuckDB
-con = duckdb.connect(database=':memory:', read_only=False)
-con.execute("CREATE SCHEMA IF NOT EXISTS csv")
+def process_sas_to_parquet(sas_filepath, metadata_filepath, output_filepath, threshold=0.5):
+    """
+    Convert a SAS dataset to Parquet format, processing it in chunks if it exceeds a specified threshold relative to 
+    the available memory.
 
-# Load metadata table
-metadata = pd.read_csv('metadata.csv')
+    Args:
+        sas_filepath (str): The filepath to the SAS dataset.
+        metadata_filepath (str): The filepath to the metadata CSV file.
+        output_filepath (str): The filepath where the output Parquet file will be saved.
+        threshold (float, optional): The threshold, as a proportion of the available memory, above which the SAS 
+            dataset will be processed in chunks. Defaults to 0.5.
 
-for _, row in metadata.iterrows():
-    # Register each lookup CSV table
-    con.execute(f"COPY csv.{row['lookup_table_name']} FROM 'lookup_{row['orig_col_name']}.csv' WITH HEADER")
+    Returns:
+        None
 
-# Load the SAS dataset
-with SAS7BDAT('mydata_encoded.sas7bdat') as f:
-    for chunk in f.readchunk(1000000):  # adjust the chunk size depending on your available memory
-        # Store the chunk in DuckDB
-        con.register('sas_chunk', chunk)
+    Raises:
+        FileNotFoundError: If the specified SAS dataset or metadata CSV file does not exist.
+        ValueError: If the threshold is not a valid float between 0 and 1.
 
-        # Iterate over the metadata and reconstruct the original dataset
+    Notes:
+        - This function assumes that the SAS dataset and metadata CSV file are compatible and have the appropriate
+          structure as expected by the implementation.
+        - The metadata CSV file should contain information about lookup tables for mapping specific columns in the 
+          SAS dataset. Each row in the metadata file should include the following columns:
+          'orig_col_name': The original column name in the SAS dataset.
+          'int_id_col_name': The column name in the lookup table used for joining.
+        - The lookup tables should be named as 'lookup_<orig_col_name>.csv' and should contain at least two columns:
+          one for the join key and another for the mapped values.
+        - The output Parquet file will contain the transformed SAS dataset with mapped values from the lookup tables.
+    """
+
+    # Load metadata table
+    metadata = pd.read_csv(metadata_filepath)
+
+    # Get the size of the dataset (in bytes)
+    dataset_size = os.path.getsize(sas_filepath)
+
+    # Get the available memory (in bytes)
+    available_memory = psutil.virtual_memory().available
+
+    if not 0 < threshold <= 1:
+        raise ValueError("Threshold must be a float between 0 and 1.")
+
+    if not os.path.exists(sas_filepath):
+        raise FileNotFoundError(f"The specified SAS dataset '{sas_filepath}' does not exist.")
+
+    if not os.path.exists(metadata_filepath):
+        raise FileNotFoundError(f"The specified metadata CSV file '{metadata_filepath}' does not exist.")
+
+    if dataset_size > threshold * available_memory:
+        # If the dataset is larger than the threshold, use chunking
+        chunksize = int(available_memory / dataset_size)  # Adjust this calculation as needed based on your specific use case
+    else:
+        # If the dataset is smaller than the threshold, load the whole dataset at once
+        chunksize = None
+
+    # Initialize empty DataFrame for the full dataset
+    full_data = pl.DataFrame()
+
+    # Load the SAS dataset in chunks (or all at once if chunksize is None)
+    for chunk in pd.read_sas(sas_filepath, chunksize=chunksize):
+        # Convert the pandas DataFrame chunk to a Polars DataFrame
+        numeric_data = pl.from_pandas(chunk)
+
+        # Lazy DataFrame operation
+        ldf = numeric_data.lazy()
+
+        # Loop through metadata to join numeric data with each lookup table
         for _, row in metadata.iterrows():
-            # Join with each lookup table on the corresponding column
-            con.execute(f"""
-                CREATE OR REPLACE VIEW sas_chunk AS
-                SELECT COALESCE(csv.{row['lookup_table_name']}.{row['orig_col_name']}, sas_chunk.{row['orig_col_name']}) as {row['orig_col_name']},
-                       sas_chunk.*
-                FROM sas_chunk
-                LEFT JOIN csv.{row['lookup_table_name']}
-                ON sas_chunk.{row['orig_col_name']} = csv.{row['lookup_table_name']}.{row['int_id_col_name']}
-            """)
+            # Lazily load each lookup table
+            lookup_table = pl.scan_csv(f"lookup_{row['orig_col_name']}.csv")
 
-        # Write out the chunk to a Parquet file
-        df = con.table('sas_chunk').to_df()
-        df.to_parquet('mydata.parquet', engine='fastparquet', append=True)
+            # Merge numeric data with lookup table
+            ldf = ldf.join(
+                lookup_table,
+                left_on=row['orig_col_name'],
+                right_on=row['int_id_col_name'],
+                how='left'
+            )
 
-        # Unregister the chunk so it doesn't take up memory
-        con.unregister('sas_chunk')
+            # Replace the original numeric column with the mapped value
+            ldf = ldf.with_column(
+                pl.col(row['orig_col_name']).fillna(pl.col(row['orig_col_name'] + '_right')).alias(row['orig_col_name'])
+            ).drop(row['orig_col_name'] + '_right')
+
+        # Collect the computation for the chunk and append to full_data
+        df_chunk = ldf.collect()
+        full_data = full_data.vstack([full_data, df_chunk])
+
+    # Write out the dataset to a Parquet file
+    full_data.write_parquet(output_filepath)
